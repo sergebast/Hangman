afa0ce9761d4ff7c62fdf85c41ff4dcd47d50bac
Sergey Bastrikov <sergeywhitedev@gmail.com>
2022-07-23 11:38:31 +0500

    feat: stage 3/8

--- hangman.py
+++ hangman.py
@@ -1,6 +1,13 @@
+import random
+
+words = ['python', 'java', 'swift', 'javascript']
+
+computer_word = random.choice(words)
+
 print("H A N G M A N")
 user_word = input("Guess the word: ")
-if user_word == 'python':
+
+if user_word == computer_word:
     print("You survived!")
 else:
     print("You lost!")
