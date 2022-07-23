627e45fda1f4e741481db774b0ec2a8d77a322bf
Sergey Bastrikov <sergeywhitedev@gmail.com>
2022-07-23 15:13:14 +0500

    feat: add clue

--- hangman.py
+++ hangman.py
@@ -1,11 +1,13 @@
-import random
+from random import choice
 
 words = ['python', 'java', 'swift', 'javascript']
 
-computer_word = random.choice(words)
+computer_word = choice(words)
+clue = computer_word[:3] + ('-' * (len(computer_word) - 3))
 
 print("H A N G M A N")
-user_word = input("Guess the word: ")
+
+user_word = input("Guess the word {}: ".format(clue))
 
 if user_word == computer_word:
     print("You survived!")
